9787ef4edf87501558f7d1730b9e48f370952b5c
= <=>
2025-12-19 06:56:02 +1100

    Big model finished training, and it certainly diffuses to blurry numbers, but it's 127 million parameters, time to reduce the bloat

--- inference_v2.py
+++ inference_v2.py
@@ -1,28 +1,27 @@
-# ======================================================================================================================
 import torch
 from modules.alpha_bar import alpha_bar_cosine
 from modules.render_image import render_image
 from modules.run_ddim_visualization import run_ddim_visualization
 
-B, C, H, W = 100, 1, 64, 64
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 from modules.s2id import SIID
 
 model = SIID(
     c_channels=1,
-    d_channels=256,
+    d_channels=512,
     rescale_factor=8,
     enc_blocks=8,
     dec_blocks=8,
-    num_heads=4,
-    pos_freq=3,
-    size_freq=3,
-    time_freq=7,
-    film_dim=256,
-    cross_dropout=0.1,
-    axial_dropout=0.1,
-    ffn_dropout=0.2,
+    num_heads=8,
+    pos_high_freq=2,
+    pos_low_freq=3,
+    time_high_freq=7,
+    time_low_freq=3,
+    film_dim=1024,
+    cross_dropout=0.05,
+    axial_dropout=0.05,
+    ffn_dropout=0.1,
     text_cond_dim=10,
     text_token_length=1,
     share_weights=False,
@@ -30,7 +29,7 @@ model = SIID(
 
 from save_load_model import load_checkpoint_into
 
-model = load_checkpoint_into(model, "models/E20_0.02602_20251218_230512.pt", "cuda")
+model = load_checkpoint_into(model, "models/big_model_512_8_8_8_8_2_3_7_3_1024.pt", "cuda")
 model.to(device)
 model.eval()
 
@@ -41,34 +40,20 @@ with torch.no_grad():
     for i in range(10):
         positive_text_conditioning[i * 10:(i + 1) * 10, i] = 1.0
 
-    small_noise = torch.randn(100, 1, 48, 48).to(device)
-    big_noise = torch.randn(100, 1, 80, 80).to(device)
+    noise = torch.randn(100, 1, 64, 64).to(device)
 
     start1 = time.time()
-    # final_x0_hat, final_x = run_ddim_visualization(
-    #     model=model,
-    #     initial_noise=small_noise,
-    #     positive_text_conditioning=positive_text_conditioning,
-    #     alpha_bar_fn=alpha_bar_cosine,
-    #     render_image_fn=render_image,
-    #     num_steps=50,
-    #     cfg_scale=1.0,  # safe
-    #     eta=1.0,
-    #     render_every=1000,
-    #     device=torch.device("cuda")
-    # )
-    start2 = time.time()
     final_x0_hat, final_x = run_ddim_visualization(
         model=model,
-        initial_noise=big_noise,
+        initial_noise=noise,
         positive_text_conditioning=positive_text_conditioning,
         alpha_bar_fn=alpha_bar_cosine,
         render_image_fn=render_image,
-        num_steps=50,
-        cfg_scale=1.5,  # safe
-        eta=1.0,
+        num_steps=25,
+        cfg_scale=3.0,
+        eta=1.5,
         render_every=1000,
         device=torch.device("cuda")
     )
     end = time.time()
-    print(f"Small: {start2 - start1:.3f} / Big: {end - start2:.3f}")
+    print(f"Time taken: {end - start1:.3f}s")
