f1b8c3b510fc5fb05e7966ea07fb7b11155802d8
Yegor-men <yegor.mn@gmail.com>
2026-02-24 08:30:07 +1100

    Updated the reference images considering the new cloud approach

--- inference_v2.py
+++ inference_v2.py
@@ -12,17 +12,16 @@ model = RIID(
     c_channels=1,
     d_channels=128,
     enc_blocks=8,
-    dec_blocks=8,
+    dec_blocks=4,
     num_heads=8,
     pos_high_freq=8,
     pos_low_freq=3,
     time_high_freq=7,
     time_low_freq=3,
     film_dim=128,
-    axial_dropout=0.1,
+    cloud_dropout=0.1,
     cross_dropout=0.1,
     ffn_dropout=0.2,
-    share_weights=False,
 )
 
 text_encoder = DummyTextCond(
@@ -32,8 +31,8 @@ text_encoder = DummyTextCond(
 
 from save_load_model import load_checkpoint_into
 
-model = load_checkpoint_into(model, "models/E20_0.03464_diffusion_20251227_061658.pt", "cuda")
-text_encoder = load_checkpoint_into(text_encoder, "models/E20_0.03464_text_embedding_20251227_061658.pt")
+model = load_checkpoint_into(model, "models/E20_0.05264_diffusion_20260224_070413.pt", "cuda")
+text_encoder = load_checkpoint_into(text_encoder, "models/E20_0.05264_text_embedding_20260224_070413.pt")
 
 model.to(device)
 model.eval()
@@ -42,50 +41,56 @@ text_encoder.to(device)
 text_encoder.eval()
 
 with torch.no_grad():
-    # positive_label = torch.zeros(100, 10).to(device)
-    # for i in range(10):
-    #     positive_label[i * 10:(i + 1) * 10, i] = 1.0
-
-    positive_label = torch.eye(10).to(device)
+    positive_label = torch.zeros(100, 10).to(device)
+    for i in range(10):
+        positive_label[i * 10:(i + 1) * 10, i] = 1.0
 
     pos_text_cond = text_encoder(positive_label)
     null_text_cond = text_encoder(torch.zeros_like(positive_label))
 
     sizes = [
-        # (128, 128, "SDXL 1MP")  # need to change the positive label code above to fit batch size 10 for memory
-        # (8, 8, "1:1"),
-        # (6, 9, "3:2"),
-        # (9, 6, "2:3"),
-        # (8, 6, "3:4"),
-        # (6, 8, "4:3"),
-        # (8, 10, "5:4"),
-        # (10, 8, "4:5"),
-        # (9, 16, "16:9"),
-        # (16, 9, "9:16"),
-        # (16, 16, "Double Resolution"),
-        # (128, 128, "Quadruple Resolution"),
-        (1024, 1024, "1MP - H:1024, W: 1024"),  # oom for batches
+        # (28, 28, 1, "Base @ 1"),
+        # (28, 28, 2, "Base @ 2"),
+        # (28, 28, 4, "Base @ 4"),
+        # (28, 28, 8, "Base @ 8"),
+        # (28, 28, 16, "Base @ 16"),
+        # (32, 32, 1, "1:1 @ 1"),
+        # (32, 32, 2, "1:1 @ 2"),
+        # (32, 32, 4, "1:1 @ 4"),
+        # (32, 32, 8, "1:1 @ 8"),
+        # (32, 32, 16, "1:1 @ 16"),
+        # (24, 40, "foo"),
+        # (40, 24, "bar"),
+        # (18, 27, "3:2"),
+        # (27, 18, "2:3"),
+        # (32, 24, "3:4"),
+        # (24, 32, "4:3"),
+        # (24, 30, "5:4"),
+        # (30, 24, "4:5"),
+        # (18, 32, "16:9"),
+        # (32, 18, "9:16"),
+        # (64, 64, 1, "Double @ 1"),
+        # (64, 64, 2, "Double @ 2"),
+        # (64, 64, 4, "Double @ 4"),
+        # (64, 64, 8, "Double @ 8"),
+        # (64, 64, 16, "Double @ 16"),
     ]
 
-    for (height, width, name) in sizes:
-        for index, thing in enumerate(positive_label):
-            thing = thing.unsqueeze(0)
-            pos_text_cond = text_encoder(thing)
-            null_text_cond = text_encoder(torch.zeros_like(thing))
+    for (height, width, num_clouds, name) in sizes:
+        grid_noise = torch.randn(100, 1, height, width).to(device)
 
-            grid_noise = torch.randn(1, 1, height, width).to(device)  # change 100 to 10 for the sdxl 1mp
-
-            final_x0_hat, final_x = run_ddim_visualization(
-                model=model,
-                initial_noise=grid_noise,
-                pos_text_cond=pos_text_cond,
-                null_text_cond=null_text_cond,
-                alpha_bar_fn=alpha_bar_cosine,
-                render_image_fn=render_image,
-                num_steps=10,
-                cfg_scale=1.0,
-                eta=1.0,
-                render_every=1,
-                device=torch.device("cuda"),
-                title=f"{name} - H:{height}, W:{width}"
-            )
+        final_x0_hat, final_x = run_ddim_visualization(
+            model=model,
+            num_clouds=num_clouds,
+            initial_noise=grid_noise,
+            pos_text_cond=pos_text_cond,
+            null_text_cond=null_text_cond,
+            alpha_bar_fn=alpha_bar_cosine,
+            render_image_fn=render_image,
+            num_steps=100,
+            cfg_scale=4.0,  # change to 1.0 for sdxl
+            eta=2.0,  # change to 1.0 for sdxl
+            render_every=1000,
+            device=torch.device("cuda"),
+            title=f"{name} - H:{height}, W:{width}"
+        )
